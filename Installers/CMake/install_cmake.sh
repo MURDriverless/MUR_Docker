0d6d66f505f5a578d8c472c2b8542cdca8d60eaa
Andrew Huang <huanga2@student.unimelb.edu.au>
2021-03-08 13:57:16 +1100

    auto download cmake

--- Installers/CMake/install_cmake.sh
+++ Installers/CMake/install_cmake.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+wget https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-Linux-x86_64.sh
 cp cmake-*.sh /opt/
 cd /opt
 sh ./cmake-* --skip-license --include-subdir
